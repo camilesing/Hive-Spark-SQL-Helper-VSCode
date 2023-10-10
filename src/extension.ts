9fce3a0ac8cd884e6ba9f3bedb5c7a665476906b
camilesing <camilesing@gmail.com>
2023-10-10 21:18:27 +0800

    add extension.ts

---
+++ src/extension.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+import { ExtensionContext } from 'vscode';
+ 
+import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+ 
+
+
+
+
+const selector = 'hive-sql'
+
+export function activate(context: ExtensionContext) {
+ 
+}
+
+ 
